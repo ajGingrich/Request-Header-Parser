6a83da420c72183a08dbf5f9533fc843f1457f0d
Andrew Gingrich <ajgingrich@yahoo.com>
2017-01-29 08:16:12 -0600

    add procfile for heroku

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var express = require('express');
+/*var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
@@ -7,4 +7,18 @@ app.get('/', function (req, res) {
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
+});*/
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+    response.send('Get some Heroku!')
+});
+
+app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'))
 });
